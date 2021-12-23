76416e8a1933256dbc4e159baa91fba170d3d450
Penuelnwaneri <penuelchudi@gmail.com>
2021-12-23 09:16:42 +0100

    updated footer,projects and writing components.

--- src/components/Projects/projects.component.jsx
+++ src/components/Projects/projects.component.jsx
@@ -1,10 +1,11 @@
-
+import { NameContainer,TitleContainer} from '../Home/home.style';
 
 
 const Project = () => (
-    <>
-    <h1>ProjectPage</h1>
-    </>
+    <div style={{opacity: 1}}>
+    <NameContainer>Projects</NameContainer>
+    <TitleContainer>I like to stay busy and always have a project in the works. Take a look at some of the applications i've dedicated my time to.</TitleContainer>
+    </div>
 )
 
 export default Project;
--- src/components/Writing/writing.component.jsx
+++ src/components/Writing/writing.component.jsx
@@ -1,11 +1,12 @@
-
+import { NameContainer,TitleContainer} from '../Home/home.style';
 
 
 
 const Writing = () => (
-    <>
-    <h1>Writing Page</h1>
-    </>
+    <div style={{opacity:1}}>
+    <NameContainer>Writing</NameContainer>
+    <TitleContainer>A collection of new things i have learned,weird bugs i have solved, new tools i try and everything in between</TitleContainer>
+    </div>
 )
 
 export default Writing;
