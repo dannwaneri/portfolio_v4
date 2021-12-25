9aba2142183dfe8451056a3719a93b4787268042
Penuelnwaneri <penuelchudi@gmail.com>
2021-12-25 04:04:15 +0100

    updated home page

--- src/components/Home/Home.component.jsx
+++ src/components/Home/Home.component.jsx
@@ -5,7 +5,7 @@ const Home = () => (
     <NameContainer>Penuel Nwaneri</NameContainer>
     <TitleContainer>Software Developer  open to both Front-end and Back-end developer roles</TitleContainer>
     <LineContainer/>
-    <TitleContainer>I care deeply about users, performance, accessibility, and having things be functional. I have an extensive experience working with techologies such as React,Node.js,Express.js,Graphql etc. Generally, i'm flexible and will work with any tool. Besides, i relish any opportunity to get my hands on a new experience!</TitleContainer>
+    <TitleContainer>I care deeply about users, performance, accessibility, and having things be functional with  React,Node.js,Express.js,Graphql.</TitleContainer>
     </div>
 )
 
