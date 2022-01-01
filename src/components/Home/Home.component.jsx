551b503c05a4599ebd87453a305309ee2cac17ee
Penuelnwaneri <penuelchudi@gmail.com>
2022-01-02 00:11:36 +0100

    edited home component

--- src/components/Home/Home.component.jsx
+++ src/components/Home/Home.component.jsx
@@ -5,7 +5,7 @@ const Home = () => (
     <NameContainer>Penuel Nwaneri</NameContainer>
     <TitleContainer>Software Developer  open to both Front-end and Back-end developer roles</TitleContainer>
     <LineContainer/>
-    <TitleContainer>He Cares deeply about users, performance, accessibility, and having things be functional;Check out my social links in the footer, or email me at</TitleContainer>
+    <TitleContainer>Penuel Cares deeply about users, performance, accessibility, and having things be functional;Check out his social links in the footer</TitleContainer>
     </div>
 )
 
