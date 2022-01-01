8337c4038161759e111372168443255ad9e440f9
Penuelnwaneri <penuelchudi@gmail.com>
2022-01-01 21:38:28 +0100

    made some changes to home component and global styles.

--- src/components/Home/Home.component.jsx
+++ src/components/Home/Home.component.jsx
@@ -5,7 +5,7 @@ const Home = () => (
     <NameContainer>Penuel Nwaneri</NameContainer>
     <TitleContainer>Software Developer  open to both Front-end and Back-end developer roles</TitleContainer>
     <LineContainer/>
-    <TitleContainer>He Cares deeply about users, performance, accessibility, and having things be functional.</TitleContainer>
+    <TitleContainer>He Cares deeply about users, performance, accessibility, and having things be functional;Check out my social links in the footer, or email me at</TitleContainer>
     </div>
 )
 
---
+++ src/constants.js
@@ -0,0 +1,113 @@
+export const COLORS = {
+    bg:{
+    light: '#FFF',
+    dark:'#000',
+    },
+    lowContrast:{ 
+      light: '#FFFFFF',
+      dark:'black',
+    },
+   highContrast:{ 
+      light: 'black',
+      dark: 'white',
+    },
+   blue1:{
+       light: 'hsl(206 100% 99.2%)',
+       dark:'hsl(212 35.0% 9.2%)',
+    },
+    blue2: {
+    light:'hsl(210 100% 98.0%)',
+    dark:'hsl(216 50.0% 11.8%)',
+    },
+  blue3:{
+      light: 'hsl(209 100% 96.5%)',
+      dark:'hsl(214 59.4% 15.3%)',
+    },
+  blue4:{
+      light:'hsl(210 98.8% 94.0%)',
+      dark:'hsl(214 65.8% 17.9%)',
+    },
+  blue5:{
+      light:'hsl(209 95.0% 90.1%)',
+      dark:'hsl(213 71.2% 20.2%);'
+    },
+    blue6: {
+        light:'hsl(209 81.2% 84.5%)',
+        dark:'hsl(212 77.4% 23.1%)',
+    },
+    blue7:{ 
+        light:' hsl(208 77.5% 76.9%)',
+        dark :'hsl(211 85.1% 27.4%)',
+    },
+    blue8: { 
+        light:'hsl(206 81.9% 65.3%)',
+        dark:'hsl(211 89.7% 34.1%)',
+    },
+    blue9:{
+        light: 'hsl(206 100% 50.0%)',
+        dark:'hsl(206 100% 50.0%)',
+    },
+    blue10:{
+        light: 'hsl(208 100% 47.3%)',
+        dark:'hsl(209 100% 60.6%)',
+    },
+    blue11: {
+        light:'hsl(211 100% 43.2%)',
+        dark:'hsl(210 100% 66.1%)',
+    },
+    blue12:{
+        light: 'hsl(211 100% 15.0%)',
+        dark:'hsl(206 98.0% 95.8%)',
+    },
+    gray1:{ 
+    light:'hsl(0 0% 99.0%)',
+    dark:'hsl(0 0% 8.5%)',
+    },
+    gray2: {
+    light: 'hsl(0 0% 97.3%)',
+    dark:  'hsl(0 0% 11.0%)',
+    },
+    gray3:{
+    light:'hsl(0 0% 95.1%)',
+    dark:'hsl(0 0% 13.6%)',
+    },
+    gray4:{
+    light:'hsl(0 0% 93.0%)',
+    dark:'hsl(0 0% 15.8%)',
+    },
+    gray5:{
+    light:'hsl(0 0% 90.9%)',
+    dark:'hsl(0 0% 17.9%)',
+    },
+    gray6:{
+    light:'hsl(0 0% 88.7%)',
+    dark:'hsl(0 0% 20.5%)',
+    },
+    gray7:{
+    light:'hsl(0 0% 85.8%)',
+    dark :'hsl(0 0% 24.3%)',
+    },
+    gray8:{
+    light:'hsl(0 0% 78.0%)',
+    dark:'hsl(0 0% 31.2%)',
+    },
+    gray9:{
+    light:' hsl(0 0% 56.1%)',
+    dark:'hsl(0 0% 43.9%)',
+    },
+    gray10:{
+        light:'hsl(0 0% 52.3%)',
+        dark:'hsl(0 0% 49.4%)',
+    },
+    gray11:{
+     light:'hsl(0 0% 43.5%)',
+     dark:'hsl(0 0% 62.8%)',   
+    },
+    gray12:{
+    light:'hsl(0 0% 9.0%)',
+    dark:'hsl(0 0% 93.0%)',
+    },
+
+};
+export const COLOR_MODE_KEY = 'color-mode';
+export const INITIAL_COLOR_MODE_CSS_PROP = '--initial-color-mode';
