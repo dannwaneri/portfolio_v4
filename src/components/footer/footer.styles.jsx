f4fd3dad3dc747b9cf4bd0bff1e3b4f01f97159f
Penuelnwaneri <penuelchudi@gmail.com>
2022-01-03 15:53:32 +0100

    added animations to footer component.

--- src/components/footer/footer.styles.jsx
+++ src/components/footer/footer.styles.jsx
@@ -1,5 +1,11 @@
-import styled from "styled-components";
+import styled,{keyframes} from "styled-components";
 
+
+
+const slideIn = keyframes`
+from{opacity:0;transform:translate(-50%,-50%) translateY(64px);}
+to{opacity:1;transform:translate(-50%,-50%) translateY(0);}
+`;
 
 export const FooterContainer = styled.footer`
     display: flex;
@@ -10,20 +16,21 @@ export const FooterContainer = styled.footer`
     padding-right: 10px;
     position: fixed;
     bottom: -8px;
+    backdrop-filter: blur(25px);
     background: var(--colors-lowContrast);
     box-shadow: var(--shadows-large);
-    backdrop-filter: blur(25px);
     border: 1px solid var(--colors-gray4);
     left: 50%;
     border-radius: 20px;
     z-index: 10;
-    opacity: 1;
-    transform: translate(-50%, -50%) translateY(0);
+    animation:${slideIn} 500ms ease forwards 200ms;
     @media (max-width: 720px) {
     width: calc(100% - 64px);
 }
 }
 `;
+
+
 
 
 export const OptionsContainer = styled.div`
