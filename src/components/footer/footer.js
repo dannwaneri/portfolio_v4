5763800bd1cdf7b824e0a41fe3f1a03d70676af2
Penuelnwaneri <penuelchudi@gmail.com>
2022-01-03 16:30:29 +0100

    updated footer component

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -66,9 +66,9 @@ return (
         >
       <motion.button  
         whileHover={{
-          scale:1.3
+          scale:2
         }}
-        whileTap={{y:-60,transition:{delay:0.2}}}
+        whileTap={{y:-50,transition:{delay:0.15}}}
       onClick={() => isClicked ? play():stop()}>
         <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
           <path opacity="0.25" d="M20 18.8V6.63998L13.6514 2.81501L13.6514 2.815C13.0511 2.45333 12.751 2.2725 12.4304 2.20186C12.1469 2.13938 11.8531 2.13938 11.5696 2.20186C11.249 2.2725 10.9489 2.45334 10.3486 2.81501L4 6.64001V18.8C4 19.9201 4 20.4802 4.21799 20.908C4.40973 21.2843 4.71569 21.5903 5.09202 21.782C5.51984 22 6.0799 22 7.2 22H16.8C17.9201 22 18.4802 22 18.908 21.782C19.2843 21.5903 19.5903 21.2843 19.782 20.908C20 20.4802 20 19.9201 20 18.8Z" fill="var(--colors-gray10)"></path><path fillRule="evenodd" clipRule="evenodd" d="M1.14251 9.5145C1.42665 9.98808 2.04091 10.1416 2.51449 9.85749L12 4.16619L21.4855 9.85749C21.9591 10.1416 22.5733 9.98808 22.8575 9.5145C23.1416 9.04092 22.9881 8.42666 22.5145 8.14251L13.029 2.45121C12.3956 2.07119 11.6044 2.07119 10.971 2.45121L1.4855 8.14251C1.01192 8.42666 0.858357 9.04092 1.14251 9.5145Z" fill="var(--colors-gray10)"></path><path d="M9 16C9 14.3431 10.3431 13 12 13C13.6569 13 15 14.3431 15 16V22H9V16Z" fill="var(--colors-gray10)"></path>
@@ -86,9 +86,9 @@ return (
         >
         <motion.button 
            whileHover={{
-            scale:1.3,
+            scale:2
           }}
-          whileTap={{y:-60,transition:{delay:0.2}}}
+          whileTap={{y:-50,transition:{delay:0.15}}}
         onClick={() => isClicked ? play():stop()}>
         <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
           <path d="M8 20C8 19.4477 8.44772 19 9 19H15C15.5523 19 16 19.4477 16 20V21C16 22.6569 14.6569 24 13 24H11C9.34315 24 8 22.6569 8 21V20Z" fill="var(--colors-gray10)">
@@ -108,9 +108,9 @@ return (
           >
           <motion.button 
           whileHover={{
-            scale:1.3
+            scale:2
           }}
-          whileTap={{y:-60,transition:{delay:0.2}}}
+          whileTap={{y:-50,transition:{delay:0.15}}}
           onClick={() => isClicked ? play():stop()}>
         <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
           <path d="M3.69601 15.8112L2.15894 19.91C1.70676 21.1158 2.88429 22.2934 4.09012 21.8412L8.18896 20.3041C8.72361 20.1036 9.20914 19.791 9.6129 19.3872L10 19L5 14L4.6129 14.3872C4.20914 14.791 3.8965 15.2765 3.69601 15.8112Z" fill="var(--colors-gray10)"></path>
@@ -131,9 +131,9 @@ return (
           >
           <motion.button 
           whileHover={{
-            scale:1.3
+            scale:2
           }}
-          whileTap={{y:-60,transition:{delay:0.2}}}
+          whileTap={{y:-50,transition:{delay:0.15}}}
           onClick={() => isClicked ? play():stop()}>
         <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
           <path opacity="0.25" d="M20.9999 7.5C21.4999 15 15.9999 21 8.99995 21C6.58804 21 4.17613 20.6768 2.28388 19.7706C1.85051 19.5631 2.0199 18.985 2.49936 18.9532C4.82944 18.7987 6.75765 18.2423 7.99995 17C11.0001 14 11.5 13 12.2646 9.02396C12.0933 8.54804 11.9999 8.03492 11.9999 7.5C11.9999 5.01472 14.0147 3 16.4999 3C18.0181 3 19.3607 3.75182 20.1757 4.90346L21.8929 4.65815C22.3207 4.59703 22.6194 5.07087 22.3796 5.43047L20.9999 7.5Z" fill="var(--colors-gray10)"></path><path d="M7.99998 16.9999C2.58358 15.1944 1.64928 8.49939 2.62226 5.00708C2.73651 4.59701 3.26964 4.59488 3.48453 4.96234C5.14601 7.80359 8.30518 9.38991 12.2646 9.02385C18.5 9.02385 17 19.9999 7.99998 16.9999Z" fill="var(--colors-gray10)"></path>
@@ -151,9 +151,9 @@ return (
           >
         <motion.button 
         whileHover={{
-          scale:1.3
+          scale:2
         }}
-        whileTap={{y:-60,transition:{delay:0.2}}}
+        whileTap={{y:-50,transition:{delay:0.15}}}
         onClick={() => isClicked ? play():stop()}>
         <svg viewBox="0 0 24 24" width="24" height="24" fill="currentColor">
         <path fill="none" d="M0 0h24v24H0z"></path>
@@ -172,9 +172,9 @@ return (
         >
       <motion.button 
       whileHover={{
-        scale:1.3
+        scale:2
       }}
-      whileTap={{y:-60,transition:{delay:0.2}}}
+      whileTap={{y:-50,transition:{delay:0.15}}}
       onClick={() => isClicked ? play():stop()}>
         <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
           <path opacity="0.25" d="M1 6C1 4.34315 2.34315 3 4 3H20C21.6569 3 23 4.34315 23 6V18C23 19.6569 21.6569 21 20 21H4C2.34315 21 1 19.6569 1 18V6Z" fill="var(--colors-gray10)"></path>
@@ -193,9 +193,9 @@ return (
       >
       <motion.button 
       whileHover={{
-        scale:1.3
+        scale:2
       }}
-      whileTap={{y:-60,transition:{delay:0.2}}}
+      whileTap={{y:-50,transition:{delay:0.15}}}
       onClick={() =>{
         isClicked ? play():stop();setTheme(nextTheme)}}>
         <svg width="48" height="48" viewBox="0 0 24 24" color="var(--colors-gray10)" fill="none" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" stroke="currentColor">
@@ -214,9 +214,9 @@ return (
       >
       <motion.button 
       whileHover={{
-        scale:1.3
+        scale:2
       }}
-      whileTap={{y:-60,transition:{delay:0.2}}}
+      whileTap={{y:-50,transition:{delay:0.15}}}
       onClick={() => {
         setIsClicked(!isClicked);
         isClicked ? play() :stop()
