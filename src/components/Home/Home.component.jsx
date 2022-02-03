621db2235b5e695c864950537553909ec1b559bd
Penuel Nwaneri <penuelnwaneri@gmail.com>
2022-02-03 13:00:28 +0100

    Update Home.component.jsx

--- src/components/Home/Home.component.jsx
+++ src/components/Home/Home.component.jsx
@@ -29,7 +29,7 @@ const Home = () => {
     animate="visible"
     exit="exit"
     >
-    <NameContainer>Penuel Nwaneri</NameContainer>
+    <NameContainer>Danny Nwaneri</NameContainer>
     <TitleContainer>Software Engineer open to both Front-end and Back-end developer roles</TitleContainer>
     <LineContainer/>
     <TitleContainer>With expertise in React, NodeJs, MongoDB, and GraphQL. He likes solving engineering problems and believes every complicated problem can be solved with ease by breaking it down to fundamental subsets.If this sounds good to you,Feel free to throw him an email!</TitleContainer>
