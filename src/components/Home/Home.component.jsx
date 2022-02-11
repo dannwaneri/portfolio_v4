f01027ecd8db1b269a75fb065e58617716d2a7ca
Danny Nwaneri <penuelnwaneri@gmail.com>
2022-02-11 18:56:58 +0100

    Update Home.component.jsx

--- src/components/Home/Home.component.jsx
+++ src/components/Home/Home.component.jsx
@@ -30,7 +30,7 @@ const Home = () => {
     exit="exit"
     >
     <NameContainer>Danny Nwaneri</NameContainer>
-    <TitleContainer>Danny Nwaneri is a Software Engineer open to both Front-end and Back-end developer roles</TitleContainer>
+    <TitleContainer>Danny Nwaneri is a Software Engineer open to both Frontend and Backend developer roles</TitleContainer>
     <LineContainer/>
     <TitleContainer>With expertise in React, NodeJs, MongoDB, and GraphQL. He likes solving engineering problems and believes every complicated problem can be solved with ease by breaking it down to fundamental subsets.</TitleContainer>
     </motion.div>
