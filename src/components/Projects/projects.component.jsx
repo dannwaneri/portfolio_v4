523d3d104ffb95bcacd2fe8fbffd3fd6bfd8bed1
dan nwaneri <penuelnwaneri@gmail.com>
2022-04-27 17:28:33 +0100

    Update projects.component.jsx

--- src/components/Projects/projects.component.jsx
+++ src/components/Projects/projects.component.jsx
@@ -27,7 +27,7 @@ const Project = () => (
     exit="exit"
     >
     <NameContainer>Projects</NameContainer>
-    <TitleContainer>I like to stay busy and always have a project in the works. Take a look at some of the applications i've dedicated my time to but it is by no means an exhaustive list of my contributions to the world.</TitleContainer>
+    <TitleContainer>Take a look at some of the applications he has dedicated his time to but it is by no means an exhaustive list of his contributions to the world.</TitleContainer>
     </motion.div>
 )
 
