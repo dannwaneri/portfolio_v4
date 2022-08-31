1ace7b289ceaf19bf7dc3d029b454933ce4e3928
Penuelnwaneri <penuelchudi@gmail.com>
2022-08-31 12:31:09 +0100

    updated

--- client/src/components/Home/Home.component.jsx
+++ client/src/components/Home/Home.component.jsx
@@ -1,6 +1,10 @@
 import {motion} from "framer-motion";
 
-import { NameContainer,TitleContainer,LineContainer} from "./home.style";
+import { 
+    NameContainer,
+    TitleContainer,
+    Title
+} from "./home.style";
 
 const containerVariants = {
     hidden:{
@@ -30,8 +34,7 @@ const Home = () => {
     exit="exit"
     >
     <NameContainer>Daniel Nwaneri</NameContainer>
-    <TitleContainer>Daniel Nwaneri is a Software Developer open to Backend developer role.</TitleContainer>
-    <LineContainer/>
+    <Title>Daniel Nwaneri is a Software Developer open to Backend developer role.</Title>
     <TitleContainer>Daniel specializes in JavaScript back-end development. He is comfortable working with many languages and platforms and has recently dedicated himself to developing back-end applications using Node.js, Express.js, MongoDB and Typescript.He also has some experience with blockchain technology and a bachelor's degree in Geophysics.</TitleContainer>
     </motion.div>
 )};
