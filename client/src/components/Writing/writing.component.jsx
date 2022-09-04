87e4a88565b3644d3f70254d874639caea3196b0
Penuelnwaneri <penuelchudi@gmail.com>
2022-09-04 03:57:07 +0100

    blog apis

--- client/src/components/Writing/writing.component.jsx
+++ client/src/components/Writing/writing.component.jsx
@@ -1,9 +1,12 @@
+import React, { useState, useEffect } from "react";
 import {motion} from "framer-motion";
 import {
     WritingHeading,
     WritingTitle,
+    Load
 } from './writing.style'
 
+import List from './List'
 
 const containerVariants = {
     hidden:{
@@ -24,7 +27,24 @@ const containerVariants = {
 }
 
 
-const Writing = () => (
+const Writing = () => {
+    const [Blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+  
+    useEffect(() => {
+      setTimeout(() => {
+        fetch('/v1/blogs')
+        .then(res => {
+          return res.json();
+        })
+        .then(data => {
+          setIsPending(false);
+          setBlogs(data);
+        })
+      }, 1000);
+    }, [])
+
+    return (
     <motion.div 
     variant={containerVariants} 
     initial="hidden"
@@ -32,8 +52,11 @@ const Writing = () => (
     exit="exit"
     >
     <WritingHeading>Writing</WritingHeading>
-    <WritingTitle>A collection of my somewhat (un)organized musings.</WritingTitle> 
+    <WritingTitle>A collection of my somewhat (un)organized musings.</WritingTitle>
+    { isPending && <Load>...</Load> }
+  {Blogs && <List Blogs={Blogs}/>}
     </motion.div>
 )
 
+}
 export default Writing;
