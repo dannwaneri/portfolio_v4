90adc83db6590e41e86de43f2c7ebe25451a9530
Penuelnwaneri <penuelchudi@gmail.com>
2022-09-04 03:58:06 +0100

    blog apis

--- routes/api.js
+++ routes/api.js
@@ -2,8 +2,11 @@ const express = require('express');
 
 const projectsRouter = require('./projects/projects.router');
 
+const blogsRouter = require('./blogs/blog.router')
+
 const api = express.Router();
 
 api.use('/projects', projectsRouter);
+api.use('/blogs', blogsRouter);
 
 module.exports = api;
