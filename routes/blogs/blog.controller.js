c4feb880d03427d56286dd6ef56ebce7818f734b
Penuelnwaneri <penuelchudi@gmail.com>
2022-10-07 23:41:23 +0100

    Porting from Create React App to Gatsby

--- routes/blogs/blog.controller.js
+++
@@ -1,9 +0,0 @@
-const {getAllBlogs} = require('../../models/blog.model')
-
-async function  httpGetAllBlogs(req, res) {
-  return res.status(200).json(await getAllBlogs());
-}
-
-module.exports = {
-  httpGetAllBlogs
-};
--- routes/projects/projects.controller.js
+++
@@ -1,9 +0,0 @@
-const {getAllProjects} = require('../../models/projects.model')
-
-async function  httpGetAllProjects(req, res) {
-  return res.status(200).json(await getAllProjects());
-}
-
-module.exports = {
-  httpGetAllProjects
-};
