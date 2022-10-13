cc6eb972be3422dc94476e64ac86d377af6ca45b
Penuelnwaneri <penuelchudi@gmail.com>
2022-10-13 02:56:57 +0100

    changes

--- gatsby-node.js
+++ gatsby-node.js
@@ -27,3 +27,19 @@ exports.createPages = async ({graphql, actions}) => {
           })
   }
 
+
+  exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+    if (stage === "build-html" || stage === "develop-html") {
+      actions.setWebpackConfig({
+        module: {
+          rules: [
+            {
+              test: /bad-module/,
+              use: loaders.null(),
+            },
+          ],
+        },
+      })
+    }
+  }
+
