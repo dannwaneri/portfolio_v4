f3a316d0335d292ed5f21af352c88272d50aa6f9
dan nwaneri <penuelnwaneri@gmail.com>
2022-10-18 08:55:15 +0100

    Update Home.component.jsx

--- src/components/Home/Home.component.jsx
+++ src/components/Home/Home.component.jsx
@@ -35,7 +35,7 @@ const Home = () => {
     exit="exit"
     >
     <NameContainer>Daniel Nwaneri</NameContainer>
-    <Title>Hey, I am Daniel Nwaneri, a Software Developer open to Backend developer role.</Title>
+    <Title>Backend Developer open to work on exciting projects.</Title>
     {/**<TitleContainer>Daniel specializes in JavaScript back-end development. He is comfortable working with many languages and platforms and has recently dedicated himself to developing back-end applications using Node.js, Express.js, MongoDB and Typescript.He also has some experience with blockchain technology and a bachelor's degree in Geophysics.</TitleContainer>**/}
     </motion.div>
 )};
