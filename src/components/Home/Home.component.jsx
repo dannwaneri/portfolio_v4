84f799d81254cd3becd788bae20ae75e9ea3641a
dan nwaneri <penuelnwaneri@gmail.com>
2022-11-14 17:05:12 +0100

    Update Home.component.jsx

--- src/components/Home/Home.component.jsx
+++ src/components/Home/Home.component.jsx
@@ -12,7 +12,7 @@ const Home = () => {
     <div>
     <NameContainer>Daniel Nwaneri</NameContainer>
     <Title>Backend Developer</Title>
-    <TitleContainer>I'm a Backend web developer with around four years of experience. I'm knowledgeable in JavaScript and Nodejs, and have plenty of other skills under my belt. If you're looking for someone to help you with your web development needs, then I'm your man! Feel free to get in touch and I'll be happy to help out. 🙂</TitleContainer>
+    <TitleContainer>I'm a Backend web developer with around four years of experience. I'm knowledgeable in JavaScript and Nodejs, and have plenty of other skills under my belt. If you're looking for someone to help you with your web development needs, then I'm your man! Feel free to get in touch and I'll be happy to help out.</TitleContainer>
     </div>
 )};
 export default Home;
